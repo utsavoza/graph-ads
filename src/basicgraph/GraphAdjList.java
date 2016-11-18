b28e22c64d19c071ffff5dc05a52ab7986b04656
utsavoza <utsavoza96@gmail.com>
2016-11-19 00:57:17 +0530

    Graph implementations

--- src/basicgraph/GraphAdjList.java
+++ src/basicgraph/GraphAdjList.java
@@ -1,42 +1,64 @@
 package basicgraph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GraphAdjList extends Graph {
 
+	private Map<Integer, ArrayList<Integer>> adjListMap;
+	
+	public GraphAdjList() {
+		adjListMap = new HashMap<Integer, ArrayList<Integer>>();
+	}
+	
 	@Override
 	public void implementAddVertex() {
-		// TODO Auto-generated method stub
-		
+		int v = getNumVertices();
+		ArrayList<Integer> neighbors = new ArrayList<Integer>();
+		adjListMap.put(v, neighbors);
 	}
 
 	@Override
 	public void implementAddEdge(int v, int w) {
-		// TODO Auto-generated method stub
-		
+		(adjListMap.get(v)).add(w);
 	}
 
 	@Override
 	public List<Integer> getNeighbors(int v) {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<Integer>(adjListMap.get(v));
 	}
 
 	@Override
 	public List<Integer> getInNeighbors(int v) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Integer> inNeighbors = new ArrayList<Integer>();
+		for(int u: adjListMap.keySet()) {
+			for(int w : adjListMap.get(u)) {
+				if(v == w) {
+					inNeighbors.add(u);
+				}
+			}
+		}
+		return inNeighbors;
 	}
 
 	@Override
 	public List<Integer> getDistance2(int v) {
-		// TODO Auto-generated method stub
+		// TODO: Implementation 4
 		return null;
 	}
 
 	@Override
 	public String adjacencyString() {
-		// TODO Auto-generated method stub
-		return null;
+		String s = "Adjacency List";
+		s += " (size " + getNumVertices() + "+" + getNumEdges() + " integers)";
+		
+		for(int v: adjListMap.keySet()) {
+			for(int w: adjListMap.get(v)) {
+				s += w+ ", ";
+			}
+		}
+		return s;
 	}
 }
--- src/basicgraph/GraphAdjMatrix.java
+++ src/basicgraph/GraphAdjMatrix.java
@@ -1,42 +1,75 @@
 package basicgraph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GraphAdjMatrix extends Graph {
 
+	private final int defaultNumVertices = 5;
+	private int[][] adjMatrix;
+	
+	public GraphAdjMatrix() {
+		adjMatrix = new int[defaultNumVertices][defaultNumVertices];
+	}
+	
 	@Override
 	public void implementAddVertex() {
-		// TODO Auto-generated method stub
-		
+		int v = getNumVertices();
+		if(v >= adjMatrix.length) {
+			int[][] newAdjMatrix = new int[v*2][v*2];
+			for(int i = 0; i < adjMatrix.length; i++) {
+				for(int j = 0;j < adjMatrix.length; j++) {
+					newAdjMatrix[i][j] = adjMatrix[i][j];
+				}
+			}
+			adjMatrix = newAdjMatrix;
+		}
 	}
 
 	@Override
 	public void implementAddEdge(int v, int w) {
-		// TODO Auto-generated method stub
-		
+		adjMatrix[v][w] += 1;
 	}
 
 	@Override
 	public List<Integer> getNeighbors(int v) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Integer> neighbors = new ArrayList<Integer>();
+		for(int i = 0; i < getNumVertices(); i++) {
+			for(int j= 0; j < adjMatrix[v][i]; j++) {
+				neighbors.add(i);
+			}
+		}
+		return neighbors;
 	}
 
 	@Override
 	public List<Integer> getInNeighbors(int v) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Integer> inNeighbors = new ArrayList<Integer>();
+		for(int i = 0; i < getNumVertices(); i++) {
+			for(int j = 0; j < adjMatrix[i][v]; j++) {
+				inNeighbors.add(i);
+			}
+		}
+		return inNeighbors;
 	}
 
 	@Override
 	public List<Integer> getDistance2(int v) {
-		// TODO Auto-generated method stub
+		// TODO: Implementation 3
 		return null;
 	}
 
 	@Override
 	public String adjacencyString() {
-		// TODO Auto-generated method stub
-		return null;
+		int dim = adjMatrix.length;
+		String s = "Adjacency Matrix";
+		s += " (size " + dim + "x" + dim + "= " + dim*dim + " integers):";
+		for(int i = 0; i < dim; i++) {
+			s += "\n\t" + i + ": ";
+			for(int j = 0; j < adjMatrix[i].length; j++) {
+				s += adjMatrix[i][j] + ", ";
+			}
+		}
+		return s;
 	}
 }
