c276e98f5250fc135984a3e6d7fed793645f99a8
utsavoza <utsavoza96@gmail.com>
2017-12-31 01:54:53 +0530

    Reformat code

--- src/basicgraph/GraphAdjList.java
+++ src/basicgraph/GraphAdjList.java
@@ -7,61 +7,61 @@ import java.util.Map;
 
 public class GraphAdjList extends Graph {
 
-	private Map<Integer, ArrayList<Integer>> adjListMap;
-	
-	public GraphAdjList() {
-		adjListMap = new HashMap<Integer, ArrayList<Integer>>();
-	}
-	
-	@Override
-	public void implementAddVertex() {
-		int v = getNumVertices();
-		ArrayList<Integer> neighbors = new ArrayList<Integer>();
-		adjListMap.put(v, neighbors);
-	}
+  private Map<Integer, ArrayList<Integer>> adjListMap;
 
-	@Override
-	public void implementAddEdge(int v, int w) {
-		(adjListMap.get(v)).add(w);
-	}
+  public GraphAdjList() {
+    adjListMap = new HashMap<Integer, ArrayList<Integer>>();
+  }
 
-	@Override
-	public List<Integer> getNeighbors(int v) {
-		return new ArrayList<Integer>(adjListMap.get(v));
-	}
+  @Override
+  public void implementAddVertex() {
+    int v = getNumVertices();
+    ArrayList<Integer> neighbors = new ArrayList<Integer>();
+    adjListMap.put(v, neighbors);
+  }
 
-	@Override
-	public List<Integer> getInNeighbors(int v) {
-		List<Integer> inNeighbors = new ArrayList<Integer>();
-		for(int u: adjListMap.keySet()) {
-			for(int w : adjListMap.get(u)) {
-				if(v == w) {
-					inNeighbors.add(u);
-				}
-			}
-		}
-		return inNeighbors;
-	}
+  @Override
+  public void implementAddEdge(int v, int w) {
+    (adjListMap.get(v)).add(w);
+  }
 
-	@Override
-	public List<Integer> getDistance2(int v) {
-		List<Integer> twoHop = new ArrayList<Integer>();
-		for(int u: getNeighbors(v)) {
-			twoHop.addAll(getNeighbors(u));
-		}
-		return twoHop;
-	}
+  @Override
+  public List<Integer> getNeighbors(int v) {
+    return new ArrayList<Integer>(adjListMap.get(v));
+  }
 
-	@Override
-	public String adjacencyString() {
-		String s = "Adjacency List";
-		s += " (size " + getNumVertices() + "+" + getNumEdges() + " integers)";
-		
-		for(int v: adjListMap.keySet()) {
-			for(int w: adjListMap.get(v)) {
-				s += w+ ", ";
-			}
-		}
-		return s;
-	}
+  @Override
+  public List<Integer> getInNeighbors(int v) {
+    List<Integer> inNeighbors = new ArrayList<Integer>();
+    for (int u : adjListMap.keySet()) {
+      for (int w : adjListMap.get(u)) {
+        if (v == w) {
+          inNeighbors.add(u);
+        }
+      }
+    }
+    return inNeighbors;
+  }
+
+  @Override
+  public List<Integer> getDistance2(int v) {
+    List<Integer> twoHop = new ArrayList<Integer>();
+    for (int u : getNeighbors(v)) {
+      twoHop.addAll(getNeighbors(u));
+    }
+    return twoHop;
+  }
+
+  @Override
+  public String adjacencyString() {
+    String s = "Adjacency List";
+    s += " (size " + getNumVertices() + "+" + getNumEdges() + " integers)";
+
+    for (int v : adjListMap.keySet()) {
+      for (int w : adjListMap.get(v)) {
+        s += w + ", ";
+      }
+    }
+    return s;
+  }
 }
--- src/basicgraph/GraphAdjMatrix.java
+++ src/basicgraph/GraphAdjMatrix.java
@@ -5,74 +5,74 @@ import java.util.List;
 
 public class GraphAdjMatrix extends Graph {
 
-	private final int defaultNumVertices = 5;
-	private int[][] adjMatrix;
-	
-	public GraphAdjMatrix() {
-		adjMatrix = new int[defaultNumVertices][defaultNumVertices];
-	}
-	
-	@Override
-	public void implementAddVertex() {
-		int v = getNumVertices();
-		if(v >= adjMatrix.length) {
-			int[][] newAdjMatrix = new int[v*2][v*2];
-			for(int i = 0; i < adjMatrix.length; i++) {
-				for(int j = 0;j < adjMatrix.length; j++) {
-					newAdjMatrix[i][j] = adjMatrix[i][j];
-				}
-			}
-			adjMatrix = newAdjMatrix;
-		}
-	}
+  private final int defaultNumVertices = 5;
+  private int[][] adjMatrix;
 
-	@Override
-	public void implementAddEdge(int v, int w) {
-		adjMatrix[v][w] += 1;
-	}
+  public GraphAdjMatrix() {
+    adjMatrix = new int[defaultNumVertices][defaultNumVertices];
+  }
 
-	@Override
-	public List<Integer> getNeighbors(int v) {
-		List<Integer> neighbors = new ArrayList<Integer>();
-		for(int i = 0; i < getNumVertices(); i++) {
-			for(int j= 0; j < adjMatrix[v][i]; j++) {
-				neighbors.add(i);
-			}
-		}
-		return neighbors;
-	}
+  @Override
+  public void implementAddVertex() {
+    int v = getNumVertices();
+    if (v >= adjMatrix.length) {
+      int[][] newAdjMatrix = new int[v * 2][v * 2];
+      for (int i = 0; i < adjMatrix.length; i++) {
+        for (int j = 0; j < adjMatrix.length; j++) {
+          newAdjMatrix[i][j] = adjMatrix[i][j];
+        }
+      }
+      adjMatrix = newAdjMatrix;
+    }
+  }
 
-	@Override
-	public List<Integer> getInNeighbors(int v) {
-		List<Integer> inNeighbors = new ArrayList<Integer>();
-		for(int i = 0; i < getNumVertices(); i++) {
-			for(int j = 0; j < adjMatrix[i][v]; j++) {
-				inNeighbors.add(i);
-			}
-		}
-		return inNeighbors;
-	}
+  @Override
+  public void implementAddEdge(int v, int w) {
+    adjMatrix[v][w] += 1;
+  }
 
-	@Override
-	public List<Integer> getDistance2(int v) {
-		List<Integer> twoHop = new ArrayList<Integer>();
-		for(int u: getNeighbors(v)) {
-			twoHop.addAll(getNeighbors(u));
-		}
-		return twoHop;
-	}
+  @Override
+  public List<Integer> getNeighbors(int v) {
+    List<Integer> neighbors = new ArrayList<Integer>();
+    for (int i = 0; i < getNumVertices(); i++) {
+      for (int j = 0; j < adjMatrix[v][i]; j++) {
+        neighbors.add(i);
+      }
+    }
+    return neighbors;
+  }
 
-	@Override
-	public String adjacencyString() {
-		int dim = adjMatrix.length;
-		String s = "Adjacency Matrix";
-		s += " (size " + dim + "x" + dim + "= " + dim*dim + " integers):";
-		for(int i = 0; i < dim; i++) {
-			s += "\n\t" + i + ": ";
-			for(int j = 0; j < adjMatrix[i].length; j++) {
-				s += adjMatrix[i][j] + ", ";
-			}
-		}
-		return s;
-	}
+  @Override
+  public List<Integer> getInNeighbors(int v) {
+    List<Integer> inNeighbors = new ArrayList<Integer>();
+    for (int i = 0; i < getNumVertices(); i++) {
+      for (int j = 0; j < adjMatrix[i][v]; j++) {
+        inNeighbors.add(i);
+      }
+    }
+    return inNeighbors;
+  }
+
+  @Override
+  public List<Integer> getDistance2(int v) {
+    List<Integer> twoHop = new ArrayList<Integer>();
+    for (int u : getNeighbors(v)) {
+      twoHop.addAll(getNeighbors(u));
+    }
+    return twoHop;
+  }
+
+  @Override
+  public String adjacencyString() {
+    int dim = adjMatrix.length;
+    String s = "Adjacency Matrix";
+    s += " (size " + dim + "x" + dim + "= " + dim * dim + " integers):";
+    for (int i = 0; i < dim; i++) {
+      s += "\n\t" + i + ": ";
+      for (int j = 0; j < adjMatrix[i].length; j++) {
+        s += adjMatrix[i][j] + ", ";
+      }
+    }
+    return s;
+  }
 }
